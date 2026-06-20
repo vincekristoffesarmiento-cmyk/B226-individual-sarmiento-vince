a33d2f5774c49a8e55bdfbc9babeccf4e90db879
Vince <vincekristoffe.sarmiento@gmail.com>
2026-06-20 16:21:56 +0800

    initial commit

---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,9 @@
+
+package day2.discussion;
+
+public class SampleCode {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/day3/discussion/SampleCode.java
@@ -0,0 +1,11 @@
+
+package day3.discussion;
+
+import day3.discussion.*;
+
+public class SampleCode {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
